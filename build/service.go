108964ced02f03ccabb674b137eaed629e7860d8
Asaf Gabai <asafg@jfrog.com>
2021-10-24 20:12:38 +0300

    Comment fixes

--- build/service.go
+++ build/service.go
@@ -25,10 +25,14 @@ func (bis *BuildInfoService) SetLogger(logger buildutils.Log) {
 	bis.logger = logger
 }
 
+// GetOrCreateBuild gets a build from cache, or creates a new one if it doesn't exist.
+// It's important to invoke this function at the very beginning of the build, so that the start time property in the build-info will be accurate.
 func (bis *BuildInfoService) GetOrCreateBuild(buildName, buildNumber string) (*Build, error) {
 	return bis.GetOrCreateBuildWithProject(buildName, buildNumber, "")
 }
 
+// GetOrCreateBuildWithProject gets a build from cache, or creates a new one if it doesn't exist.
+// It's important to invoke this function at the very beginning of the build, so that the start time property in the build-info will be accurate.
 func (bis *BuildInfoService) GetOrCreateBuildWithProject(buildName, buildNumber, projectKey string) (*Build, error) {
 	err := buildutils.SaveBuildGeneralDetails(buildName, buildNumber, projectKey, bis.tempDirPath, bis.logger)
 	if err != nil {
