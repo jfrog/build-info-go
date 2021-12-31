6713decadbd27cc2eae9e32aa3169988d9ed155a
Eyal Ben Moshe <eyalbenmoshe@jfrog.com>
2021-12-31 06:30:31 +0200

    Fix defer in tests (#30)

--- build/build_test.go
+++ build/build_test.go
@@ -47,11 +47,13 @@ func TestCollectEnv(t *testing.T) {
 	}
 
 	// Set environment variables
+	defer func() {
+		for key, _ := range env {
+			assert.NoError(t, os.Unsetenv(key))
+		}
+	}()
 	for key, value := range env {
 		assert.NoError(t, os.Setenv(key, value))
-		defer func() {
-			assert.NoError(t, os.Unsetenv(key))
-		}()
 	}
 
 	service := NewBuildInfoService()
