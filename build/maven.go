b255e90c4e976830b7bfba09e64d334dc5fbc2dd
Or Geva <orgab@jfrog.com>
2022-03-16 08:38:06 -0700

    Update build-info extractors (#70)

--- build/maven.go
+++ build/maven.go
@@ -22,7 +22,7 @@ const (
 	PropertiesTempfolderName        = "properties"
 	mavenExtractorRemotePath        = "org/jfrog/buildinfo/build-info-extractor-maven3/%s"
 	GeneratedBuildInfoTempPrefix    = "generatedBuildInfo"
-	MavenExtractorDependencyVersion = "2.33.0"
+	MavenExtractorDependencyVersion = "2.36.0"
 
 	ClassworldsConf = `main is org.apache.maven.cli.MavenCli from plexus.core
 
