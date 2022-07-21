e5dbb8a3b512b937128314f3dd6f22fd30b25a4b
Tal Arian <tala@jfrog.com>
2022-07-21 17:40:55 +0300

    Add tests

--- utils/pythonutils/poetryutils_test.go
+++ utils/pythonutils/poetryutils_test.go
@@ -1,42 +1,66 @@
 package pythonutils
 
 import (
+	"path/filepath"
+	"reflect"
+	"sort"
 	"testing"
+
+	testdatautils "github.com/jfrog/build-info-go/build/testdata"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetProjectNameFromPyproject(t *testing.T) {
 	tests := []struct {
-		pyprojectFileContent string
-		expectedProjectName  string
+		poetryProject       string
+		expectedProjectName string
 	}{
-		{"/Users/tala/dev/forks/project-examples/python-example/poetry/my-poetry-project/pyproject.toml", "my-poetry-project:0.1.0"},
-		//{`[tool.poetry]\nname = "my-poetry-project"\nversion = "0.1.0"\ndescription = ""\nauthors = ["Tal Arian <tala@jfrog.com>"]\n\n\n[tool.poetry.dependencies]\npython = "^3.10"\nnumpy = "^1.23.0"\n\n[tool.poetry.dev-dependencies]\npytest = "^5.2"\n\n[build-system]\nrequires = ["poetry-core>=1.0.0"]\nbuild-backend = "poetry.core.masonry.api"\n`, "my-poetry-project:0.1.0"},
+		{"project", "my-poetry-project:1.1.0"},
+		{"nodevdeps", "my-poetry-project:1.1.17"},
 	}
 
 	for _, test := range tests {
-		actualValue, err := ExtractProjectFromPyproject(test.pyprojectFileContent)
-		if err != nil {
-			t.Error(err)
-		}
-		if actualValue.Name != test.expectedProjectName {
-			t.Errorf("Expected value: %s, got: %s.", test.expectedProjectName, actualValue)
-		}
+		t.Run(test.poetryProject, func(t *testing.T) {
+			tmpProjectPath, cleanup := testdatautils.CreateTestProject(t, filepath.Join("..", "testdata", "poetry", test.poetryProject))
+			defer cleanup()
+
+			actualValue, err := extractProjectFromPyproject(filepath.Join(tmpProjectPath, "pyproject.toml"))
+			assert.NoError(t, err)
+			if actualValue.Name != test.expectedProjectName {
+				t.Errorf("Expected value: %s, got: %s.", test.expectedProjectName, actualValue)
+			}
+		})
 	}
 }
 
-func TestGetProjectDepsFromPoetryLock(t *testing.T) {
+func TestGetProjectDependencies(t *testing.T) {
 	tests := []struct {
-		pyprojectFileContent string
-		expectedProjectName  string
+		poetryProject                  string
+		expectedDirectDependencies     []string
+		expectedTransitiveDependencies [][]string
 	}{
-		{"/Users/tala/dev/forks/project-examples/python-example/poetry/my-poetry-project/", "my-poetry-project:0.1.0"},
+		{"project", []string{"numpy:1.23.0", "pytest:5.4.3", "python:"}, [][]string{nil, {"atomicwrites:1.4.0", "attrs:21.4.0", "colorama:0.4.5", "more-itertools:8.13.0", "packaging:21.3", "pluggy:0.13.1", "py:1.11.0", "wcwidth:0.2.5"}, nil}},
+		{"nodevdeps", []string{"numpy:1.23.0", "python:"}, [][]string{nil, nil, nil}},
 	}
 
 	for _, test := range tests {
-		_, _, err := getPoetryDependencies(test.pyprojectFileContent)
-		if err != nil {
-			t.Error(err)
-		}
+		t.Run(test.poetryProject, func(t *testing.T) {
+			tmpProjectPath, cleanup := testdatautils.CreateTestProject(t, filepath.Join("..", "testdata", "poetry", test.poetryProject))
+			defer cleanup()
 
+			graph, directDependencies, err := getPoetryDependencies(filepath.Join(tmpProjectPath))
+			assert.NoError(t, err)
+			sort.Strings(directDependencies)
+			if !reflect.DeepEqual(directDependencies, test.expectedDirectDependencies) {
+				t.Errorf("Expected value: %s, got: %s.", test.expectedDirectDependencies, directDependencies)
+			}
+			for i, directDependency := range directDependencies {
+				transitiveDependencies := graph[directDependency]
+				sort.Strings(transitiveDependencies)
+				if !reflect.DeepEqual(transitiveDependencies, test.expectedTransitiveDependencies[i]) {
+					t.Errorf("Expected value: %s, got: %s.", test.expectedTransitiveDependencies[i], graph[directDependency])
+				}
+			}
+		})
 	}
 }
