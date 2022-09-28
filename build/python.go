ab4690a33f68d637db18fe528962297af21f52f9
Tal Arian <tala@jfrog.com>
2022-09-28 11:19:08 +0300

    Add Poetry Build (#104)
    * Add poetry build support
    
    * Add bi poetry command
    
    * Handle poetry package case-insensitive names

--- build/python.go
+++ build/python.go
@@ -3,13 +3,9 @@ package build
 import (
 	"fmt"
 	"os"
-	"regexp"
-	"strings"
 
 	"github.com/jfrog/build-info-go/entities"
-	"github.com/jfrog/build-info-go/utils"
 	"github.com/jfrog/build-info-go/utils/pythonutils"
-	gofrogcmd "github.com/jfrog/gofrog/io"
 )
 
 type PythonModule struct {
@@ -33,7 +29,7 @@ func newPythonModule(srcPath string, tool pythonutils.PythonTool, containingBuil
 }
 
 func (pm *PythonModule) RunInstallAndCollectDependencies(commandArgs []string) error {
-	dependenciesMap, err := pm.InstallWithLogParsing(commandArgs)
+	dependenciesMap, err := pythonutils.GetPythonDependenciesFiles(pm.tool, commandArgs, pm.containingBuild.logger, pm.srcPath)
 	if err != nil {
 		return err
 	}
@@ -71,127 +67,7 @@ func (pm *PythonModule) RunInstallAndCollectDependencies(commandArgs []string) e
 // Run install command while parsing the logs for downloaded packages.
 // Populates 'downloadedDependencies' with downloaded package-name and its actual downloaded file (wheel/egg/zip...).
 func (pm *PythonModule) InstallWithLogParsing(commandArgs []string) (map[string]entities.Dependency, error) {
-	log := pm.containingBuild.logger
-	if pm.tool == pythonutils.Pipenv {
-		// Add verbosity flag to pipenv commands to collect necessary data
-		commandArgs = append(commandArgs, "-v")
-	}
-	installCmd := utils.NewCommand(string(pm.tool), "install", commandArgs)
-	installCmd.Dir = pm.srcPath
-
-	dependenciesMap := map[string]entities.Dependency{}
-
-	// Create regular expressions for log parsing.
-	collectingRegexp, err := regexp.Compile(`^Collecting\s(\w[\w-.]+)`)
-	if err != nil {
-		return nil, err
-	}
-	downloadingRegexp, err := regexp.Compile(`^\s*Downloading\s([^\s]*)\s\(`)
-	if err != nil {
-		return nil, err
-	}
-	usingCachedRegexp, err := regexp.Compile(`^\s*Using\scached\s([\S]+)\s\(`)
-	if err != nil {
-		return nil, err
-	}
-	alreadySatisfiedRegexp, err := regexp.Compile(`^Requirement\salready\ssatisfied:\s(\w[\w-.]+)`)
-	if err != nil {
-		return nil, err
-	}
-
-	var packageName string
-	expectingPackageFilePath := false
-
-	// Extract downloaded package name.
-	dependencyNameParser := gofrogcmd.CmdOutputPattern{
-		RegExp: collectingRegexp,
-		ExecFunc: func(pattern *gofrogcmd.CmdOutputPattern) (string, error) {
-			// If this pattern matched a second time before downloaded-file-name was found, prompt a message.
-			if expectingPackageFilePath {
-				// This may occur when a package-installation file is saved in pip-cache-dir, thus not being downloaded during the installation.
-				// Re-running pip-install with 'no-cache-dir' fixes this issue.
-				log.Debug(fmt.Sprintf("Could not resolve download path for package: %s, continuing...", packageName))
-
-				// Save package with empty file path.
-				dependenciesMap[strings.ToLower(packageName)] = entities.Dependency{Id: ""}
-			}
-
-			// Check for out of bound results.
-			if len(pattern.MatchedResults)-1 < 0 {
-				log.Debug(fmt.Sprintf("Failed extracting package name from line: %s", pattern.Line))
-				return pattern.Line, nil
-			}
-
-			// Save dependency information.
-			expectingPackageFilePath = true
-			packageName = pattern.MatchedResults[1]
-
-			return pattern.Line, nil
-		},
-	}
-
-	// Extract downloaded file, stored in Artifactory.
-	downloadedFileParser := gofrogcmd.CmdOutputPattern{
-		RegExp: downloadingRegexp,
-		ExecFunc: func(pattern *gofrogcmd.CmdOutputPattern) (string, error) {
-			// Check for out of bound results.
-			if len(pattern.MatchedResults)-1 < 0 {
-				log.Debug(fmt.Sprintf("Failed extracting download path from line: %s", pattern.Line))
-				return pattern.Line, nil
-			}
-
-			// If this pattern matched before package-name was found, do not collect this path.
-			if !expectingPackageFilePath {
-				log.Debug(fmt.Sprintf("Could not resolve package name for download path: %s , continuing...", packageName))
-				return pattern.Line, nil
-			}
-
-			// Save dependency information.
-			filePath := pattern.MatchedResults[1]
-			lastSlashIndex := strings.LastIndex(filePath, "/")
-			var fileName string
-			if lastSlashIndex == -1 {
-				fileName = filePath
-			} else {
-				fileName = filePath[lastSlashIndex+1:]
-			}
-			dependenciesMap[strings.ToLower(packageName)] = entities.Dependency{Id: fileName}
-			expectingPackageFilePath = false
-
-			log.Debug(fmt.Sprintf("Found package: %s installed with: %s", packageName, fileName))
-			return pattern.Line, nil
-		},
-	}
-
-	cachedFileParser := gofrogcmd.CmdOutputPattern{
-		RegExp:   usingCachedRegexp,
-		ExecFunc: downloadedFileParser.ExecFunc,
-	}
-
-	// Extract already installed packages names.
-	installedPackagesParser := gofrogcmd.CmdOutputPattern{
-		RegExp: alreadySatisfiedRegexp,
-		ExecFunc: func(pattern *gofrogcmd.CmdOutputPattern) (string, error) {
-			// Check for out of bound results.
-			if len(pattern.MatchedResults)-1 < 0 {
-				log.Debug(fmt.Sprintf("Failed extracting package name from line: %s", pattern.Line))
-				return pattern.Line, nil
-			}
-
-			// Save dependency with empty file name.
-			dependenciesMap[strings.ToLower(pattern.MatchedResults[1])] = entities.Dependency{Id: ""}
-			log.Debug(fmt.Sprintf("Found package: %s already installed", pattern.MatchedResults[1]))
-			return pattern.Line, nil
-		},
-	}
-
-	// Execute command.
-	var errorOut string
-	_, errorOut, _, err = gofrogcmd.RunCmdWithOutputParser(installCmd, true, &dependencyNameParser, &downloadedFileParser, &cachedFileParser, &installedPackagesParser)
-	if err != nil {
-		return nil, fmt.Errorf("failed running %s command with error: '%s - %s'", string(pm.tool), err.Error(), errorOut)
-	}
-	return dependenciesMap, nil
+	return pythonutils.InstallWithLogParsing(pm.tool, commandArgs, pm.containingBuild.logger, pm.srcPath)
 }
 
 func (pm *PythonModule) SetName(name string) {
--- utils/pythonutils/utils.go
+++ utils/pythonutils/utils.go
@@ -2,17 +2,20 @@ package pythonutils
 
 import (
 	"errors"
+	"fmt"
 	"path/filepath"
+	"regexp"
 	"strings"
 
-	buildinfo "github.com/jfrog/build-info-go/entities"
+	"github.com/jfrog/build-info-go/entities"
 	"github.com/jfrog/build-info-go/utils"
+	gofrogcmd "github.com/jfrog/gofrog/io"
 )
 
 const (
-	Pip    = "pip"
-	Pipenv = "pipenv"
-	Poetry = "poetry"
+	Pip    PythonTool = "pip"
+	Pipenv PythonTool = "pipenv"
+	Poetry PythonTool = "poetry"
 )
 
 type PythonTool string
@@ -54,6 +57,17 @@ type packageType struct {
 	InstalledVersion string `json:"installed_version,omitempty"`
 }
 
+func GetPythonDependenciesFiles(tool PythonTool, args []string, log utils.Log, srcPath string) (map[string]entities.Dependency, error) {
+	switch tool {
+	case Pip, Pipenv:
+		return InstallWithLogParsing(tool, args, log, srcPath)
+	case Poetry:
+		return extractPoetryDependenciesFiles(srcPath, args, log)
+	default:
+		return nil, errors.New(string(tool) + " commands are not supported.")
+	}
+}
+
 func GetPythonDependencies(tool PythonTool, srcPath, localDependenciesPath string) (dependenciesGraph map[string][]string, topLevelDependencies []string, err error) {
 	switch tool {
 	case Pip:
@@ -86,7 +100,7 @@ func GetPackageName(tool PythonTool, srcPath string) (packageName string, err er
 // topLevelPackagesList - The direct dependencies
 // packageName          - The resolved package name of the Python project, may be empty if we couldn't resolve it
 // moduleName           - The input module name from the user, or the packageName
-func UpdateDepsIdsAndRequestedBy(dependenciesMap map[string]buildinfo.Dependency, dependenciesGraph map[string][]string, topLevelPackagesList []string, packageName, moduleName string) {
+func UpdateDepsIdsAndRequestedBy(dependenciesMap map[string]entities.Dependency, dependenciesGraph map[string][]string, topLevelPackagesList []string, packageName, moduleName string) {
 	if packageName == "" {
 		// Projects without setup.py
 		dependenciesGraph[moduleName] = topLevelPackagesList
@@ -94,15 +108,15 @@ func UpdateDepsIdsAndRequestedBy(dependenciesMap map[string]buildinfo.Dependency
 		// Projects with setup.py
 		dependenciesGraph[moduleName] = dependenciesGraph[packageName]
 	}
-	rootModule := buildinfo.Dependency{Id: moduleName, RequestedBy: [][]string{{}}}
+	rootModule := entities.Dependency{Id: moduleName, RequestedBy: [][]string{{}}}
 	updateDepsIdsAndRequestedBy(rootModule, dependenciesMap, dependenciesGraph)
 }
 
-func updateDepsIdsAndRequestedBy(parentDependency buildinfo.Dependency, dependenciesMap map[string]buildinfo.Dependency, dependenciesGraph map[string][]string) {
+func updateDepsIdsAndRequestedBy(parentDependency entities.Dependency, dependenciesMap map[string]entities.Dependency, dependenciesGraph map[string][]string) {
 	for _, childId := range dependenciesGraph[parentDependency.Id] {
 		childName := childId[0:strings.Index(childId, ":")]
 		if childDep, ok := dependenciesMap[childName]; ok {
-			if childDep.NodeHasLoop() || len(childDep.RequestedBy) >= buildinfo.RequestedByMaxLength {
+			if childDep.NodeHasLoop() || len(childDep.RequestedBy) >= entities.RequestedByMaxLength {
 				continue
 			}
 			// Update RequestedBy field from parent's RequestedBy.
@@ -136,4 +150,127 @@ func getFilePath(srcPath, fileName string) (string, error) {
 		return "", err
 	}
 	return filePath, nil
+}
+
+func InstallWithLogParsing(tool PythonTool, commandArgs []string, log utils.Log, srcPath string) (map[string]entities.Dependency, error) {
+	if tool == Pipenv {
+		// Add verbosity flag to pipenv commands to collect necessary data
+		commandArgs = append(commandArgs, "-v")
+	}
+	installCmd := utils.NewCommand(string(tool), "install", commandArgs)
+	installCmd.Dir = srcPath
+
+	dependenciesMap := map[string]entities.Dependency{}
+
+	// Create regular expressions for log parsing.
+	collectingRegexp, err := regexp.Compile(`^Collecting\s(\w[\w-.]+)`)
+	if err != nil {
+		return nil, err
+	}
+	downloadingRegexp, err := regexp.Compile(`^\s*Downloading\s([^\s]*)\s\(`)
+	if err != nil {
+		return nil, err
+	}
+	usingCachedRegexp, err := regexp.Compile(`^\s*Using\scached\s([\S]+)\s\(`)
+	if err != nil {
+		return nil, err
+	}
+	alreadySatisfiedRegexp, err := regexp.Compile(`^Requirement\salready\ssatisfied:\s(\w[\w-.]+)`)
+	if err != nil {
+		return nil, err
+	}
+
+	var packageName string
+	expectingPackageFilePath := false
+
+	// Extract downloaded package name.
+	dependencyNameParser := gofrogcmd.CmdOutputPattern{
+		RegExp: collectingRegexp,
+		ExecFunc: func(pattern *gofrogcmd.CmdOutputPattern) (string, error) {
+			// If this pattern matched a second time before downloaded-file-name was found, prompt a message.
+			if expectingPackageFilePath {
+				// This may occur when a package-installation file is saved in pip-cache-dir, thus not being downloaded during the installation.
+				// Re-running pip-install with 'no-cache-dir' fixes this issue.
+				log.Debug(fmt.Sprintf("Could not resolve download path for package: %s, continuing...", packageName))
+
+				// Save package with empty file path.
+				dependenciesMap[strings.ToLower(packageName)] = entities.Dependency{Id: ""}
+			}
+
+			// Check for out of bound results.
+			if len(pattern.MatchedResults)-1 < 0 {
+				log.Debug(fmt.Sprintf("Failed extracting package name from line: %s", pattern.Line))
+				return pattern.Line, nil
+			}
+
+			// Save dependency information.
+			expectingPackageFilePath = true
+			packageName = pattern.MatchedResults[1]
+
+			return pattern.Line, nil
+		},
+	}
+
+	// Extract downloaded file, stored in Artifactory.
+	downloadedFileParser := gofrogcmd.CmdOutputPattern{
+		RegExp: downloadingRegexp,
+		ExecFunc: func(pattern *gofrogcmd.CmdOutputPattern) (string, error) {
+			// Check for out of bound results.
+			if len(pattern.MatchedResults)-1 < 0 {
+				log.Debug(fmt.Sprintf("Failed extracting download path from line: %s", pattern.Line))
+				return pattern.Line, nil
+			}
+
+			// If this pattern matched before package-name was found, do not collect this path.
+			if !expectingPackageFilePath {
+				log.Debug(fmt.Sprintf("Could not resolve package name for download path: %s , continuing...", packageName))
+				return pattern.Line, nil
+			}
+
+			// Save dependency information.
+			filePath := pattern.MatchedResults[1]
+			lastSlashIndex := strings.LastIndex(filePath, "/")
+			var fileName string
+			if lastSlashIndex == -1 {
+				fileName = filePath
+			} else {
+				fileName = filePath[lastSlashIndex+1:]
+			}
+			dependenciesMap[strings.ToLower(packageName)] = entities.Dependency{Id: fileName}
+			expectingPackageFilePath = false
+
+			log.Debug(fmt.Sprintf("Found package: %s installed with: %s", packageName, fileName))
+			return pattern.Line, nil
+		},
+	}
+
+	cachedFileParser := gofrogcmd.CmdOutputPattern{
+		RegExp:   usingCachedRegexp,
+		ExecFunc: downloadedFileParser.ExecFunc,
+	}
+
+	// Extract already installed packages names.
+	installedPackagesParser := gofrogcmd.CmdOutputPattern{
+		RegExp: alreadySatisfiedRegexp,
+		ExecFunc: func(pattern *gofrogcmd.CmdOutputPattern) (string, error) {
+			// Check for out of bound results.
+			if len(pattern.MatchedResults)-1 < 0 {
+				log.Debug(fmt.Sprintf("Failed extracting package name from line: %s", pattern.Line))
+				return pattern.Line, nil
+			}
+
+			// Save dependency with empty file name.
+			dependenciesMap[strings.ToLower(pattern.MatchedResults[1])] = entities.Dependency{Id: ""}
+			log.Debug(fmt.Sprintf("Found package: %s already installed", pattern.MatchedResults[1]))
+			return pattern.Line, nil
+		},
+	}
+
+	// Execute command.
+	var errorOut string
+	_, errorOut, _, err = gofrogcmd.RunCmdWithOutputParser(installCmd, true, &dependencyNameParser, &downloadedFileParser, &cachedFileParser, &installedPackagesParser)
+	if err != nil {
+		return nil, fmt.Errorf("failed running %s command with error: '%s - %s'", string(tool), err.Error(), errorOut)
+	}
+	return dependenciesMap, nil
 }
