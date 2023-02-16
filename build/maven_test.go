02f1720e673e6514c5017a60e333f334bb2f189a
Omer Zidkoni <50792403+omerzi@users.noreply.github.com>
2023-02-16 16:52:51 +0200

    Update maven_test.go

--- build/maven_test.go
+++ build/maven_test.go
@@ -49,7 +49,7 @@ func TestGenerateBuildInfoForMavenProject(t *testing.T) {
 	// Add maven project as module in build-info.
 	mavenModule, err := mavenBuild.AddMavenModule(tmpProjectPath)
 	assert.NoError(t, err)
-	mavenModule.SetMavenGoals("compile", "--no-transfer-progress", "-e")
+	mavenModule.SetMavenGoals("compile", "--no-transfer-progress")
 	// Calculate build-info.
 	err = mavenModule.CalcDependencies()
 	if assert.NoError(t, err) {
