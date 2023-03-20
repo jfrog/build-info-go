de1069540837f2deb040439c1f8067157388c264
sarao <sarao@jfrog.com>
2023-03-20 15:34:06 +0200

    Npm audit now can use tha package-lock file if exist instead of node-module.

--- build/utils/npm.go
+++ build/utils/npm.go
@@ -123,7 +123,8 @@ func runNpmLsWithNodeModules(executablePath, srcPath string, npmArgs []string, l
 	npmArgs = append(npmArgs, "--json", "--all", "--long")
 	data, errData, err := RunNpmCmd(executablePath, srcPath, Ls, npmArgs, log)
 	if err != nil {
-		log.Warn("npm list command failed with error:", err.Error())
+		// It is not mandatory for the function to return this error.
+		log.Error("npm list command failed with error:", err.Error())
 	}
 	if len(errData) > 0 {
 		log.Warn("Some errors occurred while collecting dependencies info:\n" + string(errData))
@@ -145,7 +146,7 @@ func runNpmLsWithoutNodeModules(executablePath, srcPath string, npmArgs []string
 	npmArgs = append(npmArgs, "--json", "--all", "--long", "--package-lock-only")
 	data, errData, err := RunNpmCmd(executablePath, srcPath, Ls, npmArgs, log)
 	if err != nil {
-		log.Warn("npm list command failed with error:", err.Error())
+		log.Error("npm list command failed with error:", err.Error())
 	}
 	if len(errData) > 0 {
 		log.Warn("Some errors occurred while collecting dependencies info:\n" + string(errData))
