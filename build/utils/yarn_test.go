8230595ceb86d1d8d927a585a2f0aeb84f2b8692
Eran Turgeman <81029514+eranturgeman@users.noreply.github.com>
2023-08-03 16:14:22 +0300

    Build Dependency Tree - Yarn V1 (#172)
    * updated PackageInfo struct to support Yarn v1
    
    * added functions and split in dependency tree build flow to support yarn version lesser than 2.0.0
    
    * added tests for GetYarnDependencies for Yarn 1 + added extra checks in the tests
    
    * added tests suitcase for yarn v1
    
    * changed path for Yarn v2 test suitcase
    
    * deleted old test suitcase for yarn v2 test
    
    * fixed static analysis issues
    
    * updated path for tested project example
    
    * TestGenerateBuildInfoForYarnProject : updated path for tested project and fixed test according to new package.json for this project
    
    * TestCollectDepsForYarnProjectWithTraverse, TestCollectDepsForYarnProjectWithErrorInTraverse, TestBuildYarnProjectWithArgs: updated path for tested project and fixed test according to new package.json for this project
    
    * trying test suitcase without .yarn and .yarnrc.yml
    
    * added back .yarnrc.yml and .yarn to v2 project
    
    * fixed PR notes
    
    * unified two GetVersion functions and fixed calls
    
    * deleted omitempty tags from PackageInfo fields: Name, Version
    
    * fixed all PR issues, except regexp for locators map
    
    * fixed some names
    
    * fix PR issue and optimized buildYarnV1DependencyMap
    
    * deleted yarn-1.22.1.cjs NS .yarnrc.yml in v2 test suitcase
    
    * added yarn-1.22.1.cjs NS .yarnrc.yml in v2 test suitcase
    
    * added a bug fix for missing 'yarn install' command
    
    * fixed uninstalled projects error handling and added test and suitcases
    
    * added new suitcase for testing uninstalled projects for yarn2 & yarn3
    
    * fixed wrong commit
    
    * added new suitcase for testing uninstalled projects for yarn2 & yarn3
    
    * .
    
    * .
    
    * .
    
    * deleted .DS_Stores
    
    * fixed uninstalled test to use existing suitcases and deleted redundant test suitcases
    
    * fixed static analysis issues
    
    * .
    
    * deleting comments
    
    * fixed tests according to wanted versions
    
    * fixing tests
    
    * fixing tests
    
    * moved got action from cli to build-info-go
    
    * resetting test.yml file to original state
    
    * updated test suitcases to stable versions and updated tests accordingly
    
    * trying solving failures
    
    * trying solving failures - fetched original v2 suitcase
    
    * fixed PR issues
    
    * fixed PR issues
    
    * fixed PR issues
    
    * fixed PR issues
    
    * handled edge case in splitNameAndVersion and added test for this function
    
    * switch a use of pointer to non-pointer
    
    * fixing usage in pointers
    
    * changing name of variable

--- build/utils/yarn_test.go
+++ build/utils/yarn_test.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"github.com/jfrog/build-info-go/utils"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -24,24 +26,166 @@ func TestGetYarnDependencyKeyFromLocator(t *testing.T) {
 	}
 }
 
-func TestGetYarnDependencies(t *testing.T) {
-	// Copy the project directory to a temporary directory
+func TestGetYarnV2Dependencies(t *testing.T) {
+	checkGetYarnDependencies(t, "v2", []string{"json@npm:9.0.6", "react@npm:18.2.0", "xml@npm:1.0.1"})
+}
+
+func TestBuildYarnV1Dependencies(t *testing.T) {
+	checkGetYarnDependencies(t, "v1", []string{"json@9.0.6", "react@18.2.0", "xml@1.0.1"})
+}
+
+func TestGetYarnDependenciesUninstalled(t *testing.T) {
+	checkGetYarnDependenciesUninstalled(t, "2.4.0")
+	checkGetYarnDependenciesUninstalled(t, "latest")
+}
+
+func checkGetYarnDependenciesUninstalled(t *testing.T, versionToSet string) {
 	tempDirPath, err := utils.CreateTempDir()
 	assert.NoError(t, err, "Couldn't create temp dir")
 	defer func() {
 		assert.NoError(t, utils.RemoveTempDir(tempDirPath), "Couldn't remove temp dir")
 	}()
-	testDataSource := filepath.Join("..", "testdata", "yarn")
+	testDataSource := filepath.Join("..", "testdata", "yarn", "v2")
 	testDataTarget := filepath.Join(tempDirPath, "yarn")
 	assert.NoError(t, utils.CopyDir(testDataSource, testDataTarget, true, nil))
 
 	executablePath, err := GetYarnExecutable()
 	assert.NoError(t, err)
 	projectSrcPath := filepath.Join(testDataTarget, "project")
-	dependenciesMap, root, err := GetYarnDependencies(executablePath, projectSrcPath, &PackageInfo{Name: "build-info-go-tests", Version: "v1.0.0"}, &utils.NullLog{})
+
+	err = updateDirYarnVersion(executablePath, projectSrcPath, versionToSet)
+	assert.NoError(t, err, "could not set version '"+versionToSet+"' to the test suitcase")
+
+	// Deleting yarn.lock content to make imitate the reverse action of 'yarn install'
+	lockFilePath := filepath.Join(projectSrcPath, "yarn.lock")
+	yarnLockFile, err := os.OpenFile(lockFilePath, os.O_WRONLY, 0666)
+	assert.NoError(t, err, "Could not open yarn.lock file")
+	defer func() {
+		assert.NoError(t, yarnLockFile.Close())
+	}()
+	err = yarnLockFile.Truncate(0) // This line erases the file's content without deleting the file itself
+	assert.NoError(t, err, "Could not erase yarn.lock file content")
+
+	pacInfo := PackageInfo{Name: "build-info-go-tests"}
+	_, _, err = GetYarnDependencies(executablePath, projectSrcPath, &pacInfo, &utils.NullLog{})
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "fetching dependencies failed since '"+pacInfo.Name+"' doesn't present in your lockfile\nPlease run 'yarn install' to update lockfile\n")
+}
+
+func updateDirYarnVersion(executablePath string, srcPath string, versionToSet string) (err error) {
+	command := exec.Command(executablePath, "set", "version", versionToSet)
+	command.Dir = srcPath
+	err = command.Run()
+	return
+}
+
+func checkGetYarnDependencies(t *testing.T, versionDir string, expectedLocators []string) {
+	// Copy the project directory to a temporary directory
+	tempDirPath, err := utils.CreateTempDir()
+	assert.NoError(t, err, "Couldn't create temp dir")
+	defer func() {
+		assert.NoError(t, utils.RemoveTempDir(tempDirPath), "Couldn't remove temp dir")
+	}()
+	testDataSource := filepath.Join("..", "testdata", "yarn", versionDir)
+	testDataTarget := filepath.Join(tempDirPath, "yarn")
+	assert.NoError(t, utils.CopyDir(testDataSource, testDataTarget, true, nil))
+
+	// Collecting and creating arguments for the command
+	executablePath, err := GetYarnExecutable()
+	assert.NoError(t, err)
+	projectSrcPath := filepath.Join(testDataTarget, "project")
+	pacInfo := PackageInfo{
+		Name:            "build-info-go-tests",
+		Version:         "v1.0.0",
+		Dependencies:    map[string]string{"react": "18.2.0", "xml": "1.0.1"},
+		DevDependencies: map[string]string{"json": "9.0.6"},
+	}
+	dependenciesMap, root, err := GetYarnDependencies(executablePath, projectSrcPath, &pacInfo, &utils.NullLog{})
 	assert.NoError(t, err)
 	assert.NotNil(t, root)
-	assert.True(t, strings.HasPrefix(root.Value, "build-info-go-tests@"))
-	assert.Equal(t, "v1.0.0", root.Details.Version)
-	assert.Len(t, dependenciesMap, 3)
+
+	// Checking dependencyMap
+	assert.Len(t, dependenciesMap, 6)
+	for dependencyName, depInfo := range dependenciesMap {
+		var packageCleanName, packageVersion string
+		if dependencyName != root.Value {
+			packageCleanName, packageVersion, err = splitNameAndVersion(dependencyName)
+			assert.NoError(t, err)
+			if packageCleanName == "" || packageVersion == "" {
+				t.Error("got an empty dependency name/version or in incorrect format (expected: package-name@version) ")
+			}
+		} else {
+			packageCleanName = root.Value
+		}
+
+		switch packageCleanName {
+		case "react":
+			assert.Equal(t, "18.2.0", depInfo.Details.Version)
+			assert.NotNil(t, depInfo.Details.Dependencies)
+			subDependencies := []string{"loose-envify"}
+			for _, depPointer := range depInfo.Details.Dependencies {
+				packageName, _, err := splitNameAndVersion(depPointer.Locator)
+				assert.NoError(t, err)
+				assert.Contains(t, subDependencies, packageName)
+			}
+		case "xml":
+			assert.Equal(t, "1.0.1", depInfo.Details.Version)
+			assert.Nil(t, depInfo.Details.Dependencies)
+		case "json":
+			assert.Equal(t, "9.0.6", depInfo.Details.Version)
+			assert.Nil(t, depInfo.Details.Dependencies)
+		case "loose-envify":
+			assert.NotNil(t, depInfo.Details.Dependencies)
+			assert.Len(t, depInfo.Details.Dependencies, 1)
+		case "js-tokens":
+			assert.Nil(t, depInfo.Details.Dependencies)
+		case root.Value:
+			assert.True(t, strings.HasPrefix(root.Value, "build-info-go-tests"))
+			assert.Equal(t, "v1.0.0", root.Details.Version)
+			for _, dependency := range root.Details.Dependencies {
+				assert.Contains(t, expectedLocators, dependency.Locator)
+			}
+		default:
+			t.Error("package '" + dependencyName + "' should not be inside the dependencies map")
+		}
+	}
+}
+
+func TestYarnDependency_Name(t *testing.T) {
+	testCases := []struct {
+		packageFullName     string
+		packageExpectedName string
+	}{
+		{"json@1.2.3", "json"},
+		{"@babel/highlight@7.14.0", "@babel/highlight"},
+		{"json@npm:1.2.3", "json"},
+		{"@babel/highlight@npm:7.14.0", "@babel/highlight"},
+	}
+	for _, testCase := range testCases {
+		yarnDep := YarnDependency{Value: testCase.packageFullName}
+		assert.Equal(t, testCase.packageExpectedName, yarnDep.Name())
+	}
+}
+
+func TestSplitNameAndVersion(t *testing.T) {
+	testCases := []struct {
+		packageFullName string
+		expectedName    string
+		expectedVersion string
+	}{
+		{"json@1.2.3", "json", "1.2.3"},
+		{"@babel/highlight@7.14.0", "@babel/highlight", "7.14.0"},
+		{"json@npm:1.2.3", "json", "1.2.3"},
+		{"@babel/highlight@npm:7.14.0", "@babel/highlight", "7.14.0"},
+	}
+	for _, testCase := range testCases {
+		packageCleanName, packageVersion, err := splitNameAndVersion(testCase.packageFullName)
+		assert.NoError(t, err)
+		assert.Equal(t, testCase.expectedName, packageCleanName)
+		assert.Equal(t, testCase.expectedVersion, packageVersion)
+	}
+
+	incorrectFormatPackageName := "json:1.2.3"
+	_, _, err := splitNameAndVersion(incorrectFormatPackageName)
+	assert.Error(t, err)
 }
