3dd66c2880ec5167e5a061bd85fd81ea5e483614
Yahav Itschak <yahavi@users.noreply.github.com>
2024-07-29 08:53:55 +0300

    Update Maven and Gradle extractors (#261)

--- build/gradle.go
+++ build/gradle.go
@@ -21,8 +21,8 @@ const (
 	gradleExtractorFileName           = "build-info-extractor-gradle-%s-uber.jar"
 	gradleInitScriptTemplate          = "gradle.init"
 	gradleExtractorRemotePath         = "org/jfrog/buildinfo/build-info-extractor-gradle/%s"
-	gradleExtractor4DependencyVersion = "4.33.13"
-	gradleExtractor5DependencyVersion = "5.2.0"
+	gradleExtractor4DependencyVersion = "4.33.20"
+	gradleExtractor5DependencyVersion = "5.2.3"
 	projectPropertiesFlag             = "-P"
 	systemPropertiesFlag              = "-D"
 )
