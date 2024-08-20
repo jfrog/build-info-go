bb148903e3de817b466cdc2417a91e644712432e
Asaf Ambar <asafa@jfrog.com>
2024-08-20 19:37:39 +0300

    Recognize "403 forbidden" error from mvn command output, and clasiffy it as "forbidden"  error type (#267)

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/urfave/cli/v2 v2.27.2
 	github.com/xeipuuv/gojsonschema v1.2.0
 	golang.org/x/exp v0.0.0-20240707233637-46b078467d37
+	golang.org/x/term v0.23.0
 )
 
 require (
@@ -26,6 +27,6 @@ require (
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
 	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ utils/error.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"strings"
+)
+
+type PackageManager string
+
+const (
+	Npm   PackageManager = "npm"
+	Maven PackageManager = "maven"
+	Pip   PackageManager = "pip"
+	Go    PackageManager = "go"
+)
+
+// ForbiddenError represents a 403 Forbidden error.
+type ForbiddenError struct {
+	Message string
+}
+
+// Error implements the error interface for ForbiddenError.
+func (e *ForbiddenError) Error() string {
+	return "403 Forbidden"
+}
+
+// NewForbiddenError creates a new ForbiddenError with the given message.
+func NewForbiddenError() *ForbiddenError {
+	return &ForbiddenError{}
+}
+
+// IsForbiddenOutput checks whether the provided output includes a 403 Forbidden. The various package managers have their own forbidden output formats.
+func IsForbiddenOutput(tech PackageManager, cmdOutput string) bool {
+	switch tech {
+	case "npm":
+		return strings.Contains(strings.ToLower(cmdOutput), "403 forbidden")
+	case "maven":
+		return strings.Contains(cmdOutput, "status code: 403") ||
+			strings.Contains(strings.ToLower(cmdOutput), "403 forbidden") ||
+			// In some cases mvn returns 500 status code even though it got 403 from artifactory.
+			strings.Contains(cmdOutput, "status code: 500")
+	case "pip":
+		return strings.Contains(strings.ToLower(cmdOutput), "http error 403")
+	case "go":
+		return strings.Contains(strings.ToLower(cmdOutput), "403 forbidden") ||
+			strings.Contains(strings.ToLower(cmdOutput), " 403")
+	}
+	return false
+}
