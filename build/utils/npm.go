c8b1739d445ab79e6a9ab06d9ee94615cd51be98
Connor Hindley <conn.hindley@gmail.com>
2024-09-21 05:26:30 -0600

    fix: Add scope a second time to getdeploypath (#273)

--- build/utils/npm.go
+++ build/utils/npm.go
@@ -514,7 +514,7 @@ func (pi *PackageInfo) GetDeployPath() string {
 	if pi.Scope == "" {
 		return fmt.Sprintf("%s/-/%s", pi.Name, fileName)
 	}
-	return fmt.Sprintf("%s/%s/-/%s", pi.Scope, pi.Name, fileName)
+	return fmt.Sprintf("%s/%s/-/%s/%s", pi.Scope, pi.Name, pi.Scope, fileName)
 }
 
 func (pi *PackageInfo) FullName() string {
