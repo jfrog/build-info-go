855533f6b8278fcdf6f4de655139e9a8416dba62
attiasas <assafattias93@gmail.com>
2024-12-24 17:50:20 +0300

    Pipenv graph produce not valid json

--- utils/pythonutils/pipenvutils.go
+++ utils/pythonutils/pipenvutils.go
@@ -2,6 +2,8 @@ package pythonutils
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/jfrog/gofrog/io"
 )
@@ -16,12 +18,15 @@ func getPipenvDependencies(srcPath string) (dependenciesGraph map[string][]strin
 	pipenvGraphCmd.Dir = srcPath
 	output, err := pipenvGraphCmd.RunWithOutput()
 	if err != nil {
+		err = fmt.Errorf("failed to run pipenv graph --json: %s", err.Error())
 		return
 	}
 	// Parse into array.
 	packages := make([]pythonDependencyPackage, 0)
-	err = json.Unmarshal(output, &packages)
+	// Sometimes, `pipenv graph --json` command returns output with new line characters in between (not valid json) So, we need to remove them before unmarshalling.
+	err = json.Unmarshal([]byte(strings.ReplaceAll(string(output), "\n", "")), &packages)
 	if err != nil {
+		err = fmt.Errorf("failed to parse pipenv graph --json output: %s", err.Error())
 		return
 	}
 	dependenciesGraph, topLevelDependencies, err = parseDependenciesToGraph(packages)
