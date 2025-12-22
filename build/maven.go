306c8654b0a940e119115dcdedd5e0dba7d33031
nitinp19 <nitinp@jfrog.com>
2025-12-22 21:38:02 +0530

    rebasing dev

--- build/maven.go
+++ build/maven.go
@@ -21,8 +21,7 @@ const (
 	classworldsConfFileName         = "classworlds.conf"
 	PropertiesTempFolderName        = "properties"
 	MavenExtractorRemotePath        = "org/jfrog/buildinfo/build-info-extractor-maven3/%s"
-	GeneratedBuildInfoTempPrefix    = "generatedBuildInfo"
-	MavenExtractorDependencyVersion = "2.39.5"
+	MavenExtractorDependencyVersion = "2.43.4"
 
 	ClassworldsConf = `main is org.apache.maven.cli.MavenCli from plexus.core
 
