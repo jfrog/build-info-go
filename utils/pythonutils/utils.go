81020015b3e373ec6507b9acfda9c7e7e1bc52df
nitinp19 <nitinp@jfrog.com>
2026-01-06 15:24:52 +0530

    RTECO-706 incorrect sha for pypi packages fix

--- utils/pythonutils/utils.go
+++ utils/pythonutils/utils.go
@@ -32,6 +32,7 @@ type PythonTool string
 var (
 	credentialsInUrlRegexp = regexp.MustCompile(utils.CredentialsInUrlRegexp)
 	catchAllRegexp         = regexp.MustCompile(".*")
+	canonicalPkgNameRegexp = regexp.MustCompile(`[-_.]+`)
 )
 
 // Parse pythonDependencyPackage list to dependencies map. (mapping dependency to his child deps)
@@ -166,6 +167,11 @@ func getFilePath(srcPath, fileName string) (string, error) {
 		return "", err
 	}
 	return filePath, nil
+}
+
+// canonicalizePackageName normalizes Python package name
+func canonicalizePackageName(name string) string {
+	return canonicalPkgNameRegexp.ReplaceAllString(strings.ToLower(name), "-")
 }
 
 // Create the CmdOutputPattern objects that can capture group content that may span multiple lines for logs that have line size limitations.
@@ -315,16 +321,23 @@ func InstallWithLogParsing(tool PythonTool, commandArgs []string, log utils.Log,
 			log.Debug(fmt.Sprintf("Could not resolve package name for download path: %s , continuing...", fileName))
 			return pattern.Line, nil
 		}
-		// Only save if the filename matches the expected package name pattern
+		// Only save if the filename belongs to the expected package
 		fileNameLower := strings.ToLower(fileName)
 		packageNameLower := strings.ToLower(packageName)
-		// Check if the wheel file name starts with the package name (with underscore or dash)
-		if strings.HasPrefix(fileNameLower, packageNameLower+"-") || strings.HasPrefix(fileNameLower, packageNameLower+"_") || strings.HasPrefix(fileNameLower, strings.ReplaceAll(packageNameLower, "-", "_")+"-") || strings.HasPrefix(fileNameLower, strings.ReplaceAll(packageNameLower, "_", "-")+"-") {
-			// Save dependency information.
-			dependenciesMap[packageNameLower] = entities.Dependency{Id: fileName}
-			expectingPackageFilePath = false
-			log.Debug(fmt.Sprintf("Found package: %s installed with: %s", packageName, fileName))
+		// Extract distribution name from artifact filename (segment before first '-')
+		distribution := fileNameLower
+		if dashIdx := strings.Index(distribution, "-"); dashIdx != -1 {
+			distribution = distribution[:dashIdx]
 		}
+		if canonicalizePackageName(distribution) != canonicalizePackageName(packageNameLower) {
+			log.Debug(fmt.Sprintf("Skipping download path %s because it does not match package %s", fileName, packageName))
+			return pattern.Line, nil
+		}
+
+		// Save dependency information.
+		dependenciesMap[packageNameLower] = entities.Dependency{Id: fileName}
+		expectingPackageFilePath = false
+		log.Debug(fmt.Sprintf("Found package: %s installed with: %s", packageName, fileName))
 		return pattern.Line, nil
 	}
 
