9a329ceb983f2905dc97273891e8a3a053627470
agrasth <agrasthn@jfrog.com>
2026-05-18 15:17:23 +0530

    Bump Maven extractor to 2.43.9 and Gradle4 extractor to 4.35.9

--- build/maven.go
+++ build/maven.go
@@ -22,7 +22,7 @@ const (
 	classworldsConfFileName         = "classworlds.conf"
 	PropertiesTempFolderName        = "properties"
 	MavenExtractorRemotePath        = "org/jfrog/buildinfo/build-info-extractor-maven3/%s"
-	MavenExtractorDependencyVersion = "2.43.6"
+	MavenExtractorDependencyVersion = "2.43.9"
 
 	ClassworldsConf = `main is org.apache.maven.cli.MavenCli from plexus.core
 
